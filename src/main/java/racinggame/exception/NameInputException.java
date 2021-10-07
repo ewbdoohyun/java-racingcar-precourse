bbad0a131017b430a51cb5bfd444c8695a51445d
doohyun.kim <ewb.doohyun@gmail.com>
2021-10-07 18:44:42 +0900

    refactor : 예외 클래스 정리  NameInputException 상세 설명 추가

--- src/main/java/racinggame/exception/NameInputException.java
+++ src/main/java/racinggame/exception/NameInputException.java
@@ -1,7 +1,7 @@
 package racinggame.exception;
 
 public class NameInputException extends Exception{
-    public static final String MESSAGE = "[ERROR] 이름의 패턴이 정확하지 않습니다. 각 이름은 5자 이하. ,(콤마) 구분";
+    public static final String MESSAGE = "[ERROR] 이름의 패턴이 정확하지 않습니다. 각 이름은 5자 이하. ,(콤마) 구분으로 한 두 개 이상의 이름이 필요합니다.";
     public NameInputException(){
         super(MESSAGE);
     }
