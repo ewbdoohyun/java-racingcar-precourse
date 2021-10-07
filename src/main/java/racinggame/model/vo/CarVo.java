6d9e767ee3399ba27693072f88a8b25904909e39
doohyun.kim <ewb.doohyun@gmail.com>
2021-10-07 19:48:48 +0900

    feat : 차량리스트를 입력 받아 1회 전진을 수행하는 기능 작성

--- src/main/java/racinggame/model/vo/CarVo.java
+++ src/main/java/racinggame/model/vo/CarVo.java
@@ -2,12 +2,21 @@ package racinggame.model.vo;
 
 public class CarVo {
     private String carName;
+    private int count;
 
     public CarVo(String carName) {
         this.carName = carName;
+        count = 0;
     }
 
     public String getCarName() {
         return carName;
+    }
+
+    public void forward(){
+        count++;
+    }
+    public int getCount(){
+        return count;
     }
 }
--- src/main/java/racinggame/service/RacingGameService.java
+++ src/main/java/racinggame/service/RacingGameService.java
@@ -1,6 +1,24 @@
 package racinggame.service;
 
+import nextstep.utils.Randoms;
+import racinggame.model.vo.CarListVo;
+import racinggame.model.vo.CarVo;
+
 public class RacingGameService {
 
-    
+    public CarListVo run(CarListVo carListVo){
+
+        for(CarVo carVo : carListVo.getNameList()){
+            runOneCar(carVo);
+        }
+        return carListVo;
+    }
+
+    private CarVo runOneCar(CarVo carVo){
+        int random = Randoms.pickNumberInRange(0,9);
+        if(4 <= random){
+            carVo.forward();
+        }
+        return carVo;
+    }
 }
