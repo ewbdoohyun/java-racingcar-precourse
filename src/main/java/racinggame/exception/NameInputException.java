c7ade07a9f21c92767c0cd852e94d05359bca3d3
doohyun.kim <ewb.doohyun@gmail.com>
2021-10-07 18:41:10 +0900

    refactor : 예외 클래스 정리  1. NameNotFoundException -> NameInputException  2. NameLengthExceededException -> NameInputException 로 통합

--- src/main/java/racinggame/exception/NameInputException.java
+++ src/main/java/racinggame/exception/NameInputException.java
@@ -1,8 +1,8 @@
 package racinggame.exception;
 
-public class NameNotFoundException extends Exception{
+public class NameInputException extends Exception{
     public static final String MESSAGE = "[ERROR] 이름의 패턴이 정확하지 않습니다. 각 이름은 5자 이하. ,(콤마) 구분";
-    public NameNotFoundException(){
+    public NameInputException(){
         super(MESSAGE);
     }
 }
--- src/main/java/racinggame/exception/NameLengthExceededException.java
+++
@@ -1,8 +0,0 @@
-package racinggame.exception;
-
-public class NameLengthExceededException extends Exception{
-    public static final String MESSAGE = "[ERROR] 이름이 5자를 초과했습니다.";
-    public NameLengthExceededException(){
-        super(MESSAGE);
-    }
-}
